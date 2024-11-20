916c24d1c5ecbfe3e7a201a7758148cdd6374704
Abhirama Aji Mahardhika <indonesiangamingchannel@gmail.com>
2024-11-20 21:13:27 +0700

    fix

--- app/Filament/Resources/RecipeResource/Pages/ListRecipes.php
+++ app/Filament/Resources/RecipeResource/Pages/ListRecipes.php
@@ -3,12 +3,13 @@
 namespace App\Filament\Resources\MenuResource\Pages;
 
 use App\Filament\Resources\MenuResource;
+use App\Filament\Resources\RecipeResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 
 class ListRecipes extends ListRecords
 {
-    protected static string $resource = MenuResource::class;
+    protected static string $resource = RecipeResource::class;
 
     protected function getHeaderActions(): array
     {
