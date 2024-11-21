91103a31441e963b4cb7ff323c4fd030f88bf3fc
Abhirama Aji Mahardhika <indonesiangamingchannel@gmail.com>
2024-11-21 22:36:27 +0700

    fixed diets database from varchar to text

--- database/migrations/2024_11_20_112950_create_diets_table.php
+++ database/migrations/2024_11_20_112950_create_diets_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('diets', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('description');
+            $table->text('description');
             $table->string('ingredients')->nullable()->change();
             $table->timestamps();
         });
