7cc46bdaa4e9f5ce9800e655350ff7e2c1c52c39
Abhirama Aji Mahardhika <indonesiangamingchannel@gmail.com>
2024-11-21 22:26:13 +0700

    Fixed tambah resep
    Co-authored-by: Latifmmhd <Latifmmhd@users.noreply.github.com>

--- app/Filament/Resources/RecipeResource/Pages/CreateRecipe.php
+++ app/Filament/Resources/RecipeResource/Pages/CreateRecipe.php
@@ -1,18 +1,11 @@
 <?php
 
-namespace App\Filament\Resources\MenuResource\Pages;
+namespace App\Filament\Resources\RecipeResource\Pages;
 
-use App\Filament\Resources\MenuResource;
 use App\Filament\Resources\RecipeResource;
-use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateRecipe extends CreateRecord
 {
     protected static string $resource = RecipeResource::class;
-
-    public function getTitle(): string
-    {
-        return 'Tambah Resep';
-    }
 }
--- database/migrations/2024_11_19_020809_create_recipes_table.php
+++ database/migrations/2024_11_19_020809_create_recipes_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('recipes', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('description');
+            $table->text('description');
             $table->text('instruction');
             $table->integer('dificulty_level');
             $table->binary('image');
