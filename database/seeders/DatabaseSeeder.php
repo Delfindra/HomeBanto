06fa4445c78e150dad8f279ccf01f4c7ce14723f
Daud Hidayat Ramadhan <daud28ramadhan@gmail.com>
2024-11-23 19:04:58 +0700

    memperbaiki bug seeder dan merapihkan navigasi
    Signed-off-by: Daud Hidayat Ramadhan <daud28ramadhan@gmail.com>

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,11 +3,12 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\PermissionRegistrar;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,7 +18,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         app()[PermissionRegistrar::class]->forgetCachedPermissions();
-
+        $this->call(ShieldSeeder::class);
         $permissions = ['edit Post', 'delete Post', 'publish Post'];
         foreach ($permissions as $permission) {
             Permission::firstOrCreate(['name' => $permission]);
@@ -26,7 +27,7 @@ class DatabaseSeeder extends Seeder
         $userRole = Role::firstOrCreate(['name' => 'user']);
         $editorRole = Role::firstOrCreate(['name' => 'editor']);
 
-        $editorRole->givePermissionTo(['name'=>'edit Post']);
+        $editorRole->givePermissionTo(['name' => 'edit Post']);
         $adminRole->givePermissionTo($permissions);
 
         $admin = User::factory()->create([
@@ -36,12 +37,12 @@ class DatabaseSeeder extends Seeder
         $admin->assignRole($adminRole);
 
 
-        $user =User::factory()->create([
+        $user = User::factory()->create([
             'email' => 'test@example.com',
         ]);
 
         $user->assignRole($userRole);
-        $editor =User::factory()->create([
+        $editor = User::factory()->create([
             'name' => 'Editor',
             'email' => 'editor@example.com'
         ]);
