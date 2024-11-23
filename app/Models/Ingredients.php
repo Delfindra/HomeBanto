e518565e0a1a2eda07d569735f3a2eef80958682
anjeliputeri <96364462+anjeliputeri@users.noreply.github.com>
2024-11-23 18:16:31 +0700

    add dashboard widget

--- app/Models/Ingredients.php
+++ app/Models/Ingredients.php
@@ -61,4 +61,27 @@ class Ingredients extends Model
             }
         });
     }
+
+    // Update the status when fetching ingredients
+    public static function boot()
+    {
+        parent::boot();
+
+        // Listen for saving event
+        static::saving(function ($ingredient) {
+            $expiryDate = Carbon::parse($ingredient->expiry_date);
+            $currentDate = Carbon::now();
+
+            $daysLeft = $expiryDate->diffInDays($currentDate, false);
+
+            // Update the status based on the expiry date
+            if ($expiryDate->lt($currentDate)) {
+                $ingredient->status = 'Expired (' . abs(intval($daysLeft)) . ' days ago)';
+            } elseif ($expiryDate->lte($currentDate->addDays(7))) {
+                $ingredient->status = 'Nearly Expired (' . abs(intval($daysLeft)) . ' days left)';
+            } else {
+                $ingredient->status = 'Fresh (' . abs(intval($daysLeft)) . ' days left)';
+            }
+        });
+    }
 }
