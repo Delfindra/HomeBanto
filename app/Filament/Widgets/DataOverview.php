1e699c9259b4676f3bb2d2c9e89e2540d7b3975c
anjeliputeri <96364462+anjeliputeri@users.noreply.github.com>
2024-11-23 22:09:08 +0700

    fix dashboard

--- app/Filament/Widgets/DataOverview.php
+++ app/Filament/Widgets/DataOverview.php
@@ -21,11 +21,10 @@ class DataOverview extends BaseWidget
         $ingredientsStock = Ingredients::where('users_id', $user->id)
             ->where('quantity', '<=', 5)
             ->count();
-
         return [
             Stat::make('Ingredients Item', $ingredientsCount),
-            Stat::make('Ingredients Near Expiry', $ingredientsExpiry),
             Stat::make('Low Stock Ingredients', $ingredientsStock),
+            Stat::make('Ingredients Near Expiry', $ingredientsExpiry),
         ];
     }
 }
--- app/Models/Menu.php
+++ app/Models/Menu.php
@@ -7,12 +7,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Menu extends Model
 {
-    use HasFactory;
-
-    protected $fillable = [
-        'name', 'description', 'intruction', 'cooking_time', 'diffcutly_level', 'image',
-    ];
-
     protected $guarded = [
         'id',
     ];
