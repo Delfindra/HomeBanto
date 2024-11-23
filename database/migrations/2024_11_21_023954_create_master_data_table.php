3d600bb987129b1553c0c74223f6700bca51a194
Daud Hidayat Ramadhan <daud28ramadhan@gmail.com>
2024-11-23 18:38:26 +0700

    memperbaiki migrate master_data
    Signed-off-by: Daud Hidayat Ramadhan <daud28ramadhan@gmail.com>

--- database/migrations/2024_11_21_023954_create_master_data_table.php
+++ database/migrations/2024_11_21_023954_create_master_data_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -22,8 +21,15 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
+    public function down()
     {
+        // Disable foreign key checks
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0;');
+
+        // Drop the table
         Schema::dropIfExists('master_data');
+
+        // Re-enable foreign key checks
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1;');
     }
 };
