ee6c0f65f5db9ec3fc7bec69d0edee73c001ceaf
Abhirama Aji Mahardhika <indonesiangamingchannel@gmail.com>
2024-11-23 19:25:25 +0700

    fix conflict merge

--- app/Models/Ingredients.php
+++ app/Models/Ingredients.php
@@ -89,6 +89,7 @@ class Ingredients extends Model
         return $query->where('type', 'expense'); // Ganti 'type' sesuai kolom yang relevan
     }
 
+
     public function scopeIncomes($query)
     {
         return $query->where('type', 'income'); // Ganti 'type' sesuai kolom yang relevan
