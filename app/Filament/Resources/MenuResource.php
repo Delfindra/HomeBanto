675733cd7a991510285fd2560aa5523acbd7009a
Abhirama Aji Mahardhika <indonesiangamingchannel@gmail.com>
2024-11-24 22:34:31 +0700

    Fix recommended recipe query

--- app/Filament/Resources/MenuResource.php
+++ app/Filament/Resources/MenuResource.php
@@ -15,6 +15,7 @@ use Filament\Tables\Table;
 use Filament\Tables\Filters\SelectFilter;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Schema;
 
 class MenuResource extends Resource implements HasShieldPermissions
 {
@@ -43,38 +44,6 @@ class MenuResource extends Resource implements HasShieldPermissions
         return false;
     }
 
-
-    // public static function form(Form $form): Form
-    // {
-    //     return $form
-    //         ->schema([
-    //             Forms\Components\TextInput::make('name')
-    //                 ->required(),
-    //             Forms\Components\TextInput::make('description')
-    //                 ->required(),
-    //             Forms\Components\Textarea::make('intruction')
-    //                 ->required()
-    //                 ->rows(10)
-    //                 ->cols(20),
-    //             Forms\Components\TextInput::make('cooking_time')
-    //                 ->required()
-    //                 ->numeric(),
-    //             Forms\Components\TextInput::make('diffcutly_level')
-    //                 ->required(),
-    //             Forms\Components\Select::make('ingredients')
-    //                 ->label('Ingredients')
-    //                 ->multiple()
-    //                 ->options(options: ingredients::all()->pluck('name', 'id'))
-    //                 ->searchable()
-    //                 ->required(),
-    //             Forms\Components\FileUpload::make('image')
-    //                 ->image()
-    //                 ->disk('public')
-    //                 ->preserveFilenames()
-    //                 ->required(),
-    //         ]);
-    // }
-
     public static function table(Table $table): Table
     {
         $availableIngredients = Ingredients::all()->pluck('name')->toArray();
@@ -90,28 +59,32 @@ class MenuResource extends Resource implements HasShieldPermissions
 
         $query = recipes::query();
 
-        // Build the query to check for available ingredients
-        if (!empty($availableIngredients)) {
+        // Check if the recipes table has the 'ingredient' column
+        if (Schema::hasColumn('recipes', 'ingredient')) {
             // Build the query to check for available ingredients
-            $query->where(function ($subQuery) use ($availableIngredients) {
-                foreach ($availableIngredients as $ingredient) {
-                    $subQuery->orWhere('ingredient', 'like', '%' . $ingredient . '%');
-                }
-            });
+            if (!empty($availableIngredients)) {
+                $query->where(function ($subQuery) use ($availableIngredients) {
+                    foreach ($availableIngredients as $ingredient) {
+                        $subQuery->orWhere('ingredient', 'like', '%' . $ingredient . '%');
+                    }
+                });
+            } else {
+                $query->whereRaw('1 = 0'); // If no available ingredients, return an empty query
+            }
+
+            // Exclude recipes that contain allergens
+            if (!empty($allergyIngredients)) {
+                $query->where(function ($subQuery) use ($allergyIngredients) {
+                    foreach ($allergyIngredients as $allergen) {
+                        $subQuery->where('ingredient', 'not like', '%' . $allergen . '%');
+                    }
+                });
+            }
         } else {
-            // If there are no available ingredients, return an empty query
+            // Handle the case where the 'ingredient' column does not exist
+            // You might want to return an empty result set or log an error
             $query->whereRaw('1 = 0'); // This will always evaluate to false
         }
-
-        // Exclude recipes that contain allergens
-        if (!empty($allergyIngredients)) {
-            $query->where(function ($subQuery) use ($allergyIngredients) {
-                foreach ($allergyIngredients as $allergen) {
-                    $subQuery->where('ingredient', 'not like', '%' . $allergen . '%');
-                }
-            });
-        }
-
 
         return $table
             ->heading('Recommended Recipe (Available Ingredient + Allergies)')
@@ -199,8 +172,6 @@ class MenuResource extends Resource implements HasShieldPermissions
     {
         return [
             'index' => Pages\ListMenus::route('/'),
-            //'create' => Pages\CreateMenu::route('/create'),
-            //'edit' => Pages\EditMenu::route('/{record}/edit'),
         ];
     }
 }
